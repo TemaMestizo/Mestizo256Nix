b3bb9689efbf293aa55e7f0b0b87c9f660d8cd89
anfigeno <dolorcriticodevastador@proton.me>
2026-02-07 13:31:40 -0500

    Primer intento de arreglar la opciónd epaleta

--- src/modulo.nix
+++ src/modulo.nix
@@ -4,14 +4,11 @@
 
   options.mestizo256nix = with lib.types; {
     activar = lib.mkEnableOption "Activar mestizo256";
-    paleta =
-      lib.range 1 255
-      |> map (n: {
-        "color${builtins.toString n}" = {
-          type = types.str;
-          description = "Uno de los 256 colores de la paleta";
-        };
-      })
-      |> lib.mergeAttrsList;
+    paleta = types.attrsOf (
+      lib.mkOption {
+        type = types.str;
+        description = "Uno de los 256 colores de la paleta";
+      }
+    );
   };
 }
