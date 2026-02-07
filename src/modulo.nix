e5c82ee2bf42403cceb0b3b2a56d1011493be72f
anfigeno <dolorcriticodevastador@proton.me>
2026-02-07 13:38:59 -0500

    Cambio del tipo de la opción `paleta`

--- src/modulo.nix
+++ src/modulo.nix
@@ -5,12 +5,7 @@
   options.mestizo256nix = with lib.types; {
     activar = lib.mkEnableOption "Activar mestizo256";
     paleta = lib.mkOption {
-      type = types.attrsOf (
-        lib.mkOption {
-          type = types.str;
-          description = "Uno de los 256 colores de la paleta";
-        }
-      );
+      type = types.attrsOf types.anything;
       description = "La paleta a usar";
     };
   };
