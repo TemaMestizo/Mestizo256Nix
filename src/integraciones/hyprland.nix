28e76ce420a434f61e45a1fa6f919590a2ddb6c3
anfigeno <dolorcriticodevastador@proton.me>
2026-03-23 14:47:25 -0500

    Cambiamos sinceramente no se muy bien que estoy ahciendo

--- src/integraciones/hyprland.nix
+++ src/integraciones/hyprland.nix
@@ -12,7 +12,7 @@ in
       let
         f = cadena: "rgb(${builtins.substring 1 (-1) cadena})";
         colorActivo = f cfg.paleta.color1;
-        colorInactivo = f cfg.paleta.color242;
+        colorInactivo = f cfg.paleta.color238;
         colorActivoBloqueado = f cfg.paleta.color2;
         colorInactivoBloqueado = f cfg.paleta.color118;
       in
@@ -29,7 +29,7 @@ in
           "col.border_locked_inactive" = colorInactivoBloqueado;
           groupbar = {
             "col.active" = colorActivo;
-            "col.inactive" = colorInactivo;
+            "col.inactive" = f color0;
             "col.locked_active" = colorActivoBloqueado;
             "col.locked_inactive" = colorInactivoBloqueado;
             text_color = f color252;
